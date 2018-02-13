6f48ab474b0755b9a56bef7164c4c5f8cdfcae38
Stephanie <stephanie4104@gmail.com>
2018-02-12 21:20:13 -0500

    Add li sortable with saving order in ajax

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css"> 
+    <link rel="stylesheet" href="css/base.css">
+
+  </head>
+  <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Maison</li>
+      <li><a href="index.php">List</a></li>
+      <li><a href="add.php">Add</a></li>
+    </ul>
+  </div>
+</div>
+
+  <article class="grid-container">
+    <div class="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
+  <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maison_sort";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo'<ul id="sortable" class="sortable ui-sortable">';
+$sql = "SELECT * FROM sort_save ORDER BY display_order ASC ";
+$result = $conn->query($sql);
+$test = 0;
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      
+     echo'<li id="item-'.$row['id'].'">
+     <div class="card">
+     <img src="http://via.placeholder.com/350x350">
+     <div class="card-section">
+       <h4>Maison</h4>
+       <p>'.$row['todo'].'</p>
+     </div>
+   </div>
+</li>';
+
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</ul>
+
+</div>
+
+
+  </article>
+
+</div>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- jQuery UI -->
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="js/foundation.js"> </script>
+<script>
+ $(document).ready(function () {
+  $('ul').sortable({
+     // axis: 'y',
+      stop: function (event, ui) {
+        var data = $(this).sortable('serialize');
+         // $('span').text(data);
+          $.ajax({
+                  data: data,
+              type: 'POST',
+              url: 'refresh_order.php'
+          });
+}
+  });
+
+  $(document).foundation();
+});
+
+
+     </script>
+
+</html>
---
+++ refresh_order.php
@@ -0,0 +1,87 @@
+<?php
+ 
+ $conn=mysqli_connect("localhost","root","","maison_sort");
+ 
+ // Check connection
+ if (mysqli_connect_errno())
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+// $_POST['item'] = $data;
+//error_log($data);
+
+$i = 0;
+
+foreach ($_POST['item'] as $value) {
+    // Execute statement:
+
+    $sql = 'UPDATE sort_save SET display_order = '.$i.' WHERE id = '.$value;
+        // $sql = "UPDATE sort_save SET id = $i WHERE id = $id";
+    
+    //error_log($sql);
+    if(mysqli_query($conn, $sql)) {
+        	    error_log( "Record modified successfully.");
+                 } else {
+         	    error_log( "ERROR: Could not execute $sql. ");
+                 }
+    $i++;
+}
+ 
+//  $move = $desired_position > $current_position ? 'down' : 'up';
+//  error_log($move);
+
+//  // Set the display_order for the dragged item to be 0 so we can update this record later by display_order = 0
+// $query = "UPDATE sort_save
+// SET display_order = 0
+// WHERE display_order = :current_position
+// AND user_id = :user_id";
+
+// // Move down: Update the items between the current position and the desired position, decreasing each item by 1 to make space for the new item
+// if ($move == 'down') {
+//     $query = "UPDATE sort_save
+//               SET display_order = (display_order - 1)
+//               WHERE display_order > :current_position
+//               AND display_order <= :desired_position
+//               AND user_id = :user_id";
+// }
+
+// // Move up: Update the items between the desired position and the current position, increasing each item by 1 to make space for the new item
+// if ($move == 'up') {
+//     $query = "UPDATE sort_save
+//               SET display_order = (display_order + 1)
+//               WHERE display_order >= :desired_position
+//               AND display_order < :current_position
+//               AND user_id = :user_id";
+// }
+
+// // Update the item that was dragged and set it to be the desired position now that the slot is opend up
+// $query = "UPDATE sort_save
+// SET display_order = :desired_position
+// WHERE display_order = 0
+// AND user_id = :user_id";
+
+
+// if(mysqli_query($conn, $query)) {
+//     	    echo "Record modified successfully.";
+//             } else {
+//         echo "ERROR: Could not execute $query. ";
+//             }
+// // Your code here to connect to MySQL server
+//     $i = 0;
+//     foreach ($_POST['item'] as $value) {
+//         // Sanitize the input
+//         $id = mysqli_real_escape_string($conn, $value);
+//         // Build statement:
+//         $sql = "UPDATE sort_save SET id = $i WHERE id = $id";
+//         // Execute statement:
+//         if(mysqli_query($conn, $sql)) {
+// 	    echo "Record modified successfully.";
+//         } else {
+// 	    echo "ERROR: Could not execute $sql. " . mysqli_error($link);
+//         }
+//         $i++;
+//     }
+ 
+// Close MySQL connection
+?>
