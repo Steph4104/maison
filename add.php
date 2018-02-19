fe7d1facb8d544c683d71286c19de92aea3ca0d5
Stephanie <stephanie4104@gmail.com>
2018-02-18 20:17:26 -0500

    add edit section

--- add.php
+++ add.php
@@ -31,82 +31,130 @@
     <div class="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
  
 </ul>
-<form action='save_data.php' method="POST" enctype="multipart/form-data">
+<?php
+if(isset($_GET['action'])){
+  if ($_GET['action'] == 'edit'){
+    $action = 'edit_data.php';
+  }else{
+    $action = 'save_data.php';
+  }
+}else{
+  $action = 'save_data.php';
+}
+
+if(isset($_GET['house_id'])){
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "maison_sort";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
+  
+  $id =$_GET['house_id'];
+  
+      $info = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM info_maison WHERE id = $id"));
+  
+}else{
+  $info['link'] = '';
+  $info['prix'] = '';
+  $info['chambre'] = '';
+  $info['bain'] = '';
+  $info['autobus'] = '';
+  $info['habitable'] = '';
+  $info['adresse'] = '';
+  $info['img'] = '';
+  $info['taxe_m'] = '';
+  $info['taxe_s'] = '';
+  $info['inclusion'] = '';
+  $info['exclusion'] = '';
+  $info['pour'] = '';
+  $info['contre'] = '';
+  $info['autre'] = '';
+}
+
+?>
+<form action='<?php echo $action; ?>' method="POST" enctype="multipart/form-data">
   <div class="grid-container">
+  <input type="number" name='id' placeholder="id" class="hide" value="<?php echo (isset($id)) ?$id : ''; ?>">    
     <div class="grid-x grid-padding-x">
       <div class="medium-6 cell">
         <label>Lien
-          <input type="url" name='lien' placeholder="lien">
+          <input type="url" name='lien' placeholder="lien" value="<?php echo $info['link']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Prix
-          <input type="number" name='prix' placeholder="prix">
+          <input type="number" name='prix' placeholder="prix" value="<?php echo $info['prix']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Nmb de chambre
-          <input type="number" name='chambre' placeholder="chambre">
+          <input type="number" name='chambre' placeholder="chambre" value="<?php echo $info['chambre']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Salle de bain
-          <input type="number" name='bain' placeholder="salle de bain">
+          <input type="number" name='bain' placeholder="salle de bain" value="<?php echo $info['bain']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Temps en autobus
-          <input type="number" name="autobus" placeholder="autobus">
+          <input type="number" name="autobus" placeholder="autobus" value="<?php echo $info['autobus']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Surface habitable
-          <input type="number" name='habitable' placeholder="p2">
+          <input type="number" name='habitable' placeholder="p2"  value="<?php echo $info['habitable']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Adresse
-          <input type="text" name='adresse' placeholder="adresse">
+          <input type="text" name='adresse' placeholder="adresse"  value="<?php echo $info['adresse']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label for="fileToUpload">Upload an image</label>
-        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="file" name="fileToUpload" id="fileToUpload" value="<?php echo $info['img']; ?>">
        
       </div>
       <div class="medium-6 cell">
         <label>Taxes municipales
-          <input type="number" name='taxe_m' placeholder="taxe">
+          <input type="number" name='taxe_m' placeholder="taxe"  value="<?php echo $info['taxe_m']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Taxes scolaires
-          <input type="number" name='taxe_s' placeholder="taxe">
+          <input type="number" name='taxe_s' placeholder="taxe"  value="<?php echo $info['taxe_s']; ?>">
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Inclusion
-        <textarea name='inclusion' placeholder="None"></textarea>
+        <textarea name='inclusion' placeholder="None"> <?php echo $info['inclusion']; ?></textarea>
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Exclusion
-        <textarea name='exclusion' placeholder="None"></textarea>
+        <textarea name='exclusion' placeholder="None"><?php echo $info['exclusion']; ?></textarea>
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Commentaires
-        <textarea name='commentaire' placeholder="None"></textarea>
+        <textarea name='commentaire' placeholder="None"><?php echo $info['autre']; ?></textarea>
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Point pour
-        <textarea name='pour' placeholder="None"></textarea>
+        <textarea name='pour' placeholder="None"><?php echo $info['pour']; ?></textarea>
         </label>
       </div>
       <div class="medium-6 cell">
         <label>Point contre
-        <textarea name='contre' placeholder="None"></textarea>
+        <textarea name='contre' placeholder="None"><?php echo $info['contre']; ?></textarea>
         </label>
       </div>
     </div>
---
+++ edit_data.php
@@ -0,0 +1,86 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maison_sort";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+    $lien = ($_POST['lien']) ? $_POST['lien'] : 'N/A';
+    $prix = ($_POST['prix']) ? $_POST['prix'] : 0;
+    $chambre = ($_POST['chambre']) ? $_POST['chambre'] : 0;
+    $bain = ($_POST['bain']) ? $_POST['bain'] : 0;
+    $autobus = ($_POST['autobus']) ? $_POST['autobus'] : 0;
+    $habitable = ($_POST['habitable']) ? $_POST['habitable'] : 0;
+    $adresse = ($_POST['adresse']) ? $_POST['adresse'] : 'N/A';
+    $taxe_m = ($_POST['taxe_m']) ? $_POST['taxe_m'] : 0;    
+    $taxe_s = ($_POST['taxe_s']) ? $_POST['taxe_s'] : 0;
+    $inclusion = ($_POST['inclusion']) ? $_POST['inclusion'] : 'N/A';    
+    $exclusion = ($_POST['exclusion']) ? $_POST['exclusion'] : 'N/A';
+    $pour = ($_POST['pour']) ? $_POST['pour'] : 'N/A';
+    $contre = ($_POST['contre']) ? $_POST['contre'] : 'N/A';
+    $comment = ($_POST['commentaire']) ? $_POST['commentaire'] : 'N/A';
+    $id = ($_POST['id']) ? $_POST['id'] : '';
+    
+if($_FILES["fileToUpload"]['error'] != 4){ // File should always exit but error 4 mean nothing was uplaod
+    $target_dir = "image_house/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+
+        if ($_FILES["fileToUpload"]["size"] == 0) {
+            echo "Sorry, your file is too large.";
+            error_log("Sorry, your file is too large.");
+            $uploadOk = 0;
+        }
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        }
+        
+        if ($uploadOk == 0) {
+            error_log("Error, can't upload");
+            header('Location: add.php');
+            exit;
+    
+        }else{
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                $sql_img = "UPDATE info_maison SET  img = '$target_file'  WHERE id = '$id'"; // if file change, upload the new one
+
+                if ($conn->query($sql_img) === TRUE) {
+                    error_log("New record created successfully");
+                    
+                } else {
+                    error_log( "Error: " . $sql_img . "<br>" . $conn->error);
+                    echo "Error: " . $sql_img . "<br>" . $conn->error;
+                    echo "Something went wrong :( (img_edit)";
+                }
+   
+            }
+        }
+    }
+}
+
+$inclusion = mysqli_real_escape_string($conn, $inclusion);
+   $sql = "UPDATE info_maison SET link = '$lien', prix = '$prix', chambre = '$chambre', bain = '$bain', autobus = '$autobus', habitable = '$habitable', taxe_m = '$taxe_m', taxe_s = '$taxe_m', inclusion = '$inclusion', exclusion = '$exclusion', pour = '$pour', contre = '$contre', autre = '$comment'  WHERE id = '$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        error_log("New record created successfully");
+        header('Location: add.php');
+        
+    } else {
+        error_log( "Error: " . $sql . "<br>" . $conn->error);
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Something went wrong :( (Info)";
+    }
+
+?>
