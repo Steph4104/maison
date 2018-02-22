8dbf6f5c1917b4e85780dbcaaed80a300f35564b
Stephanie <stephanie4104@gmail.com>
2018-02-22 16:53:14 -0500

    can upload empty field without error

--- save_data.php
+++ save_data.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
     $autobus = ($_POST['autobus']) ? $_POST['autobus'] : 0;
     $habitable = ($_POST['habitable']) ? $_POST['habitable'] : 0;
     $adresse = ($_POST['adresse']) ? $_POST['adresse'] : 'N/A';
-    $year = ($_POST['year']) ? $_POST['year'] : 'N/A';    
+    $year = ($_POST['year']) ? $_POST['year'] : 0;    
     $taxe_m = ($_POST['taxe_m']) ? $_POST['taxe_m'] : 0;    
     $taxe_s = ($_POST['taxe_s']) ? $_POST['taxe_s'] : 0;
     $inclusion = ($_POST['inclusion']) ? $_POST['inclusion'] : 'N/A';    
@@ -26,7 +26,7 @@ if ($conn->connect_error) {
     $pour = ($_POST['pour']) ? $_POST['pour'] : 'N/A';
     $contre = ($_POST['contre']) ? $_POST['contre'] : 'N/A';
     $comment = ($_POST['commentaire']) ? $_POST['commentaire'] : 'N/A';
-
+    
 if($_FILES["fileToUpload"]){
     $target_dir = "image_house/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -34,34 +34,39 @@ if($_FILES["fileToUpload"]){
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
-
-        if ($_FILES["fileToUpload"]["size"] == 0) {
-            echo "Sorry, your file is too large.";
-            error_log("Sorry, your file is too large.");
-            $uploadOk = 0;
-        }
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
-        }
-        
-        if ($uploadOk == 0) {
-            error_log("Error, can't upload");
-            header('Location: add.php');
-            exit;
-    
-        }else{
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-            }
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
         }
     }
-}else{
-    $target_file = 'N/A';
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 0) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
 }
 $inclusion = mysqli_real_escape_string($conn, $inclusion);
-   $sql = "INSERT INTO info_maison (link,chambre,prix,habitable,img,taxe_s,taxe_m,bain,autobus,adresse,year,inclusion,exclusion,pour,contre,autre) VALUES ('$lien','$chambre','$prix','$habitable','$target_file','$taxe_s','$taxe_m','$bain','$autobus','$adresse','$year','$inclusion','$exclusion','$pour','$contre','$comment')";
+   $sql = "INSERT INTO info_maison (link,chambre,prix,habitable,img,taxe_s,taxe_m,bain,autobus,adresse,year,inclusion,exclusion,pour,contre,autre,sold) VALUES ('$lien','$chambre','$prix','$habitable','$target_file','$taxe_s','$taxe_m','$bain','$autobus','$adresse','$year','$inclusion','$exclusion','$pour','$contre','$comment','dispo')";
 
     if ($conn->query($sql) === TRUE) {
         error_log("New record created successfully");
