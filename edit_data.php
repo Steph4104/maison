c00a61e3603f8d92c7143a1a2c5c30a1ed2336d5
Stephanie <stephanie4104@gmail.com>
2018-03-13 21:45:22 -0400

    fix taxe_s

--- edit_data.php
+++ edit_data.php
@@ -9,16 +9,16 @@ include 'database.php';
     $autobus = ($_POST['autobus']) ? $_POST['autobus'] : 0;
     $habitable = ($_POST['habitable']) ? $_POST['habitable'] : 0;
     $adresse = ($_POST['adresse']) ? $_POST['adresse'] : 'N/A';
-    $year = ($_POST['year']) ? $_POST['year'] : 0;    
-    $taxe_m = ($_POST['taxe_m']) ? $_POST['taxe_m'] : 0;    
+    $year = ($_POST['year']) ? $_POST['year'] : 0;
+    $taxe_m = ($_POST['taxe_m']) ? $_POST['taxe_m'] : 0;
     $taxe_s = ($_POST['taxe_s']) ? $_POST['taxe_s'] : 0;
-    $inclusion = ($_POST['inclusion']) ? $_POST['inclusion'] : 'N/A';    
+    $inclusion = ($_POST['inclusion']) ? $_POST['inclusion'] : 'N/A';
     $exclusion = ($_POST['exclusion']) ? $_POST['exclusion'] : 'N/A';
     $pour = ($_POST['pour']) ? $_POST['pour'] : 'N/A';
     $contre = ($_POST['contre']) ? $_POST['contre'] : 'N/A';
     $comment = ($_POST['commentaire']) ? $_POST['commentaire'] : 'N/A';
     $id = ($_POST['id']) ? $_POST['id'] : '';
-    
+
 if($_FILES["fileToUpload"]['error'] != 4){ // File should always exit but error 4 mean nothing was uplaod
     $target_dir = "image_house/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -37,12 +37,12 @@ if($_FILES["fileToUpload"]['error'] != 4){ // File should always exit but error 
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         }
-        
+
         if ($uploadOk == 0) {
             error_log("Error, can't upload");
             header('Location: add.php');
             exit;
-    
+
         }else{
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
@@ -50,13 +50,13 @@ if($_FILES["fileToUpload"]['error'] != 4){ // File should always exit but error 
 
                 if ($conn->query($sql_img) === TRUE) {
                     error_log("New record created successfully");
-                    
+
                 } else {
                     error_log( "Error: " . $sql_img . "<br>" . $conn->error);
                     echo "Error: " . $sql_img . "<br>" . $conn->error;
                     echo "Something went wrong :( (img_edit)";
                 }
-   
+
             }
         }
     }
@@ -69,13 +69,13 @@ $contre = mysqli_real_escape_string($conn, $contre);
 $comment = mysqli_real_escape_string($conn, $comment);
 $adresse = mysqli_real_escape_string($conn, $adresse);
 
-   $sql = "UPDATE info_maison SET link = '$lien', prix = '$prix', chambre = '$chambre', bain = '$bain', autobus = '$autobus', adresse = '$adresse', year = '$year', habitable = '$habitable', taxe_m = '$taxe_m', taxe_s = '$taxe_m', inclusion = '$inclusion', exclusion = '$exclusion', pour = '$pour', contre = '$contre', autre = '$comment'  WHERE id = '$id'";
+   $sql = "UPDATE info_maison SET link = '$lien', prix = '$prix', chambre = '$chambre', bain = '$bain', autobus = '$autobus', adresse = '$adresse', year = '$year', habitable = '$habitable', taxe_m = '$taxe_m', taxe_s = '$taxe_s', inclusion = '$inclusion', exclusion = '$exclusion', pour = '$pour', contre = '$contre', autre = '$comment'  WHERE id = '$id'";
 
     if ($conn->query($sql) === TRUE) {
         error_log("New record created successfully");
         $_SESSION['success'] = 'edit';
         header('Location: index.php');
-        
+
     } else {
         error_log( "Error: " . $sql . "<br>" . $conn->error);
         echo "Error: " . $sql . "<br>" . $conn->error;
